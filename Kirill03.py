ad835f67424cad9156bc979ac43d2e8fd13b1c9b
KirillKor20 <vwer7743@mail.ru>
2024-05-28 18:15:06 +0300

    Add files via upload

---
+++ Kirill03.py
@@ -0,0 +1,4 @@
+The_number_of_completed_DZ = '12'
+Number_of_hours_spent = '1.5'
+Course_name = 'Курс: Python'
+print (Course_name,",всего задач:",The_number_of_completed_DZ,',затрачено часов:',Number_of_hours_spent,',среднее время выполнение 0.125 часа.')
